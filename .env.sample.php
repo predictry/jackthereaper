c79f55797dc45ed39555e1ffde430b3b57acc350
Rifki Yandhi <rifkiyandhi@gmail.com>
2014-12-24 15:03:58 +0800

    add tracking bucket and log

--- .env.sample.php
+++ .env.sample.php
@@ -7,14 +7,16 @@
  * Function     : 
  */
 return array(
-    'ENCRYPTION_KEY'   => 'super-secret-sauce',
-    'MYSQL_HOST'       => 'mysqlhost',
-    'MYSQL_DATABASE'   => 'mysqldatabase',
-    'MYSQL_USERNAME'   => 'mysqlusername',
-    'MYSQL_PASSWORD'   => 'mysqlpassword',
-    "SQS_QUEUE_KEY"    => "sqs-queue-key",
-    "SQS_QUEUE_SECRET" => "sqs-queue-secret",
-    "SQS_QUEUE_URL"    => "sqs-queue-url",
-    "SQS_QUEUE_REGION" => "sqs-queue-region",
-    "DEBUG_MODE"       => false
+    'ENCRYPTION_KEY'              => 'super-secret-sauce',
+    'MYSQL_HOST'                  => 'mysqlhost',
+    'MYSQL_DATABASE'              => 'mysqldatabase',
+    'MYSQL_USERNAME'              => 'mysqlusername',
+    'MYSQL_PASSWORD'              => 'mysqlpassword',
+    "SQS_QUEUE_KEY"               => "sqs-queue-key",
+    "SQS_QUEUE_SECRET"            => "sqs-queue-secret",
+    "SQS_QUEUE_URL"               => "sqs-queue-url",
+    "SQS_QUEUE_REGION"            => "sqs-queue-region",
+    "TRACKING_BUCKET"             => "",
+    "TRACKING_BUCKET_ACCESS_LOGS" => "",
+    "DEBUG_MODE"                  => false
 );
