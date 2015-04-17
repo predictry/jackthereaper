97240a1153549bbf8e13a8fcc65a6d2366bd45c7
Rifki Yandhi <rifkiyandhi@gmail.com>
2015-04-17 11:34:07 +0800

    add extract object and download object methods

--- app/commands/LogsBaseCommand.php
+++ app/commands/LogsBaseCommand.php
@@ -78,6 +78,40 @@ class LogsBaseCommand extends Command
         }
     }
 
+    function extractObject($keyname)
+    {
+        // Raising this value may increase performance
+        $buffer_size   = 4096; // read 4kb at a time
+        $out_file_name = str_replace('.gz', '', $keyname);
+
+        // Open our files (in binary mode)
+        $file     = gzopen($keyname, 'rb');
+        $out_file = fopen($out_file_name, 'wb');
+
+        // Keep repeating until the end of the input file
+        while (!gzeof($file)) {
+            // Read buffer-size bytes
+            // Both fwrite and gzread and binary-safe
+            fwrite($out_file, gzread($file, $buffer_size));
+        }
+
+        // Files are done, close files
+        fclose($out_file);
+        gzclose($file);
+    }
+
+    function downloadObject($bucket, $keyname, $dest)
+    {
+        // Save object to a file.
+        $result = $this->s3->getObject(array(
+            'Bucket' => $bucket,
+            'Key'    => $keyname,
+            'SaveAs' => $dest
+        ));
+
+        return ($result) ? true : false;
+    }
+
 }
 
 /* End of file LogsBaseCommand.php */
